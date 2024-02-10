f74f7a1a3d533c54f38b682c56ec95826bb2491f
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 14:41:50 +0500

    setup smptlib with constants

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+import smtplib
 
+GMAIL_USER = 'mawwabkhank2006@gmail.com'
+GMAIL_PASSWORD = os.getenv('GMAIL_PASSWORD')
 url = 'https://www.cyoa.com/collections/bestsellers/products/time-travel-inn'
 
 response = requests.get(url=url)
@@ -8,7 +12,5 @@ response.raise_for_status()
 webpage = response.text
 
 soup = BeautifulSoup(webpage, 'html.parser')
-price = soup.find('span', id='price-field')
-price = price.find('span', class_='money').text
-
-print(price)
+price_tag = soup.find('span', id='price-field')
+price = float(price_tag.find('span', class_='money').text.strip('$'))
